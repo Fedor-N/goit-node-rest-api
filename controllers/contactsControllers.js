2eefaee674bb00a38c13660f8ec812e87ba0d4ca
Fedor-N <nfedorf@gmail.com>
2025-03-31 00:47:20 +0300

    +

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,60 @@
-import contactsService from "../services/contactsServices.js";
+import * as contactsService from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+    try {
+        const contacts = await contactsService.listContacts();
+        res.status(200).json(contacts);
+    }
+    catch (err) {
+        next(err);
+    }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const contact = await contactsService.getContactById(id);
+        if (!contact) {
+            return res.status(404).json({ message: "Not found" });
+        }
+        res.status(200).json(contact);
+    } catch (err) {
+        next(err);
+    }
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const contact = await contactsService.removeContact(id);
+        if (!contact) {
+            return res.status(404).json({ message: "Not found" });
+        }
+        res.status(200).json(contact);
+    } catch (err) {
+        next(err);
+    }
+};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res, next) => {
+    try {
+        const newContact = await contactsService.addContact(req.body);
+        res.status(201).json(newContact);
+    } catch (err) {
+        next(err);
+    }
+};
 
-export const updateContact = (req, res) => {};
+export const updateContact = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const contact = await contactsService.getContactById(id);
+        if (!contact) {
+            return res.status(404).json({ message: "Not found" });
+        }
+        const updatedContact = await contactsService.updateContact(id, req.body);
+        res.status(200).json(updatedContact);
+    } catch (err) {
+        next(err);
+    }
+};
